b33515e210929f8beee70d7afa4f45d00d849f72
xbg <2602576227@qq.com>
2021-10-08 17:25:15 +0800

    init repo

---
+++ src/main/java/com/xbg/community/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.xbg.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+* @className : HelloController
+* @description :
+* @author : Administrator
+* @date: 2021/10/8 16:54
+*/
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
